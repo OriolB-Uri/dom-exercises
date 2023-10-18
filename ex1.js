957b572a80feb17e1d0752cfd4be290d2e850ab5
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-18 12:07:50 +0200

    ex hechos

--- ex1.js
+++ ex1.js
@@ -7,11 +7,14 @@
 
 /**
  * Cambia el título de la página a rojo usando un estilo en línea
+ * 
  */
+document.querySelector("h1").style.color = "red";
 
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
  */
+document.querySelector("h1").style.fontSize = "48px";
 
 /**
  * Muestra por console.log el texto de la caja 2
@@ -20,16 +23,18 @@
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
+document.querySelector("p.intro").style.borderColor = "green";
 
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
+document.querySelector("#ultimo").classList.remove("estilo-3");
 
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo
  * Ayuda: https://simpledev.io/lesson/add-class-js/
  * 
  * BONUS: Haz el ejercicio sin modificar el ex1.html
- 
  */
+document.querySelector(".estilo-2").classList.add("ocultar");
